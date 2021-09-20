2489881b8e60b983d519c0481639c5262c3cd307
air2310 <angela.renton@uqconnect.edu.au>
2021-09-20 05:12:39 +0000

    testing

--- main_ATTNNF.py
+++ main_ATTNNF.py
@@ -20,6 +20,7 @@ import RSA as RSA
 
 # TODO: During NF stats. -NF flicker
 # TODO: Get working on Neurodesk
+# TODO: Get working on neurodesk again. 
 # TODO: Statistical tests for wavelets (time in each state)
 # TODO: individual trial independance of spatial and feature based-attention neurofeedback - how correlated are they really?
 
